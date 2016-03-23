b9383aab1f91bee02a078fd59199e66195b3a9ce
Alexander Flyax <alexander.flyax@uptake.com>
2016-03-23 16:05:52 -0500

    install changes, print statements

--- install_script.sh
+++ install_script.sh
@@ -3,12 +3,23 @@ scp -r ch1data02.uptake.com:/home/common/ANN_talk_vision/*.csv .
 
 # for py 2.7:
 
-pip install -r https://raw.githubusercontent.com/dnouri/nolearn/master/requirements.txt
-pip install git+https://github.com/dnouri/nolearn.git@master#egg=nolearn==0.7.git
+echo "if asked, wipe or backup"
+echo "this will take a while"
+echo "your local password:"
+
+sudo pip uninstall nolearn
+sudo pip uninstall lasagne
+
+# sudo pip install -r https://raw.githubusercontent.com/dnouri/nolearn/master/requirements.txt https://github.com/dnouri/nolearn/archive/master.zip#egg=nolearn
+
+echo "this will take a while too:"
+sudo conda install scikit-learn
+sudo pip install -r https://raw.githubusercontent.com/dnouri/nolearn/master/requirements.txt
+sudo pip install git+https://github.com/dnouri/nolearn.git@master#egg=nolearn==0.7.git
 
 
 # for both:
 
-pip install seaborn
-pip install pandas
-pip install nolearn
+sudo pip install seaborn
+sudo pip install pandas
+sudo pip install nolearn
