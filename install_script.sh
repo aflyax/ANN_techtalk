7d86482a6429a5c2fb9547e03233ad771fd9461c
Alexander Flyax <alexander.flyax@uptake.com>
2016-03-23 13:17:23 -0500

    first commit

---
+++ install_script.sh
@@ -0,0 +1,11 @@
+# for py 2.7:
+
+pip install -r https://raw.githubusercontent.com/dnouri/nolearn/master/requirements.txt
+pip install git+https://github.com/dnouri/nolearn.git@master#egg=nolearn==0.7.git
+
+
+# for both:
+
+pip install seaborn
+pip install pandas
+pip install nolearn
