0f938270c793be1b3549418ebf5105eee3f568b9
Alexander Flyax <aflyax@gmail.com>
2016-03-25 13:32:01 -0500

    theano upgrades for 2.7

--- install_script.sh
+++ install_script.sh
@@ -25,4 +25,7 @@ sudo pip install seaborn
 sudo pip install pandas
 sudo pip install nolearn
 
+pip install --upgrade https://github.com/Theano/Theano/archive/master.zip
+pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
+
 jupyter notebook
